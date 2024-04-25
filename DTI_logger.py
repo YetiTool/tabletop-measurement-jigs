43b82984041134f7dc65025f705d3f74929f5a3a
Loz Procter <lozprocter@yahoo.co.uk>
2024-04-25 18:13:02 +0100

    formatting changes to XY coord output including rounding to 3dp

--- DTI_logger.py
+++ DTI_logger.py
@@ -4,13 +4,17 @@ from datetime import datetime
 import os
 import threading
 
+# User defined variables
+x_gridpoints = 3  # For data formatting change to number of x_gridpoints in probing_gcode_generator
+PORT = 'COM14'
+
 dt_string = datetime.now().strftime("%d-%m-%Y %H-%M-%S")
 z_coords_filename = f"Z Vals {dt_string}.csv"
 path = os.path.dirname(os.path.abspath(__file__))
 readings = 0
 reading = 0
 
-PORT = 'COM14'
+# Serial connection to DTI setup
 ser = serial.Serial(PORT, 9600, timeout=5)
 time.sleep(1)
 ser.flushInput()
@@ -18,6 +22,7 @@ ser.flushInput()
 def get_reading():
     try:
         raw_reading = ser.read_until(b'\r',20)
+        # print(raw_reading)
         reading = float(raw_reading[3:])
         reading /= 1000.0
         return reading
@@ -42,5 +47,5 @@ with open (local_path_z_coords, 'w') as f:
         print(value)
         print(readings)
         f.write(f"{value},")
-        if readings % 3 == 0:
+        if readings % x_gridpoints == 0:
              f.write("\n")
--- probing_gcode_generator.py
+++ probing_gcode_generator.py
@@ -17,18 +17,18 @@ port = 22
 
 # gcode params
 # x_coords = [0, 404, 900]
-x_min, x_max, x_gridpoints = 0, 1200, 4  # change these values for x reach and gridpoints
-y_min, y_max, y_gridpoints = 0, 2400, 8  # change these values for y reach and gridpoints
-xy_feedrate = 4000
+x_min, x_max, x_gridpoints = 0, 1140, 5  # change these values for x reach and gridpoints
+y_min, y_max, y_gridpoints = 0, 2020, 10  # change these values for y reach and gridpoints
+xy_feedrate = 6000
 z_feedrate = 300
-dwell_period = 5
+dwell_period = 3
 
 # coordinate logging
 xy_coords_list = []
 
 # gcode generator
-x_coords = np.linspace(x_min, x_max, num=x_gridpoints + 1)
-y_coords = np.linspace(y_max, y_min, num=y_gridpoints + 1)
+x_coords = np.linspace(x_min, x_max, num=x_gridpoints)
+y_coords = np.linspace(y_max, y_min, num=y_gridpoints)
 local_path_gcode = os.path.join(path, "Output", gcode_filename)
 with open(local_path_gcode, 'w') as f:
     f.write("G0\n")
@@ -37,11 +37,11 @@ with open(local_path_gcode, 'w') as f:
 
     for y in y_coords:
         for x in x_coords:
-            f.write(f"X{x} Y{y} F{xy_feedrate}\n")
+            f.write(f"X{x:.3f} Y{y:.3f} F{xy_feedrate}\n")
             f.write(f"Z-3 F{z_feedrate}\n")
             f.write(f"G4 P{dwell_period}\n")
             f.write(f"Z10 F{z_feedrate}\n")
-            xy_coords_list.append(f"{x}, {y}")
+            xy_coords_list.append(f"{x:.3f}, {y:.3f},@")
 
     f.write(f"Z30 F{z_feedrate}\n")
     print(f"File: '{gcode_filename}' saved to '{local_path_gcode}'")
@@ -51,10 +51,14 @@ f.close()
 # coordinate log generator
 local_path_xy_coords = os.path.join(path, "Output", xy_coords_filename)
 with open (local_path_xy_coords, 'w') as c:
+    coords = 0
     for item in xy_coords_list:
-        c.write(f"{str(item)}\n")
-    print(f"File: '{xy_coords_filename}' saved to '{local_path_xy_coords}'")
+        c.write(f"{str(item)}")
+        coords += 1
+        if coords % x_gridpoints == 0:
+            c.write("\n")
 c.close()
+print(f"File: '{xy_coords_filename}' saved to '{local_path_xy_coords}'")
 
 # sftp sending gcode file to console
 ssh_client = paramiko.SSHClient()
